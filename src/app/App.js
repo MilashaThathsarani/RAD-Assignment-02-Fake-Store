3b1f3be57856a7da40fd9a620d534aec52fce98c
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-09-12 22:51:42 +0530

    Updated

--- src/app/App.js
+++ src/app/App.js
@@ -1,23 +1,31 @@
 import {Route, Routes} from "react-router-dom";
+import Product from "../pages/Product";
 import LoginAdmin from "../pages/session/Login";
-import localStorageService from "../service/localStorageService";
+import Dashboard from "../pages/Dashboard";
+import Cart from "../pages/Cart";
+import Register from "../pages/Register";
 import NavBar from "../component/common/NavBar";
-import React from "react";
+import localStorageService from "../service/localStorageService";
 
 function App() {
-  return (<>
-        <header style={{display:localStorageService.getItem("user")?"block":"none"}}>
-          <NavBar/>
-        </header>
-        <main>
-          <Routes>
-            <Route exact path="/" element={<LoginAdmin/>}/>
+    return (<>
+            <header style={{display:localStorageService.getItem("user")?"block":"none"}}>
+                <NavBar/>
+            </header>
+            <main>
+                <Routes>
+                    <Route exact path="/" element={<LoginAdmin/>}/>
 
-          </Routes>
-        </main>
-      </>
+                    {/*<Route exact path="/" element={<LoginAdmin/>}/>*/}
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                    <Route path="/product" element={<Product/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/register" element={<Register/>}/>
+                </Routes>
+            </main>
+        </>
 
-  );
+    );
 }
 
 export default App;
